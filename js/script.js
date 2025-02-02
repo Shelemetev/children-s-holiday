14d76f927a0e3974f7e216417257ed60ffc45b03
Egor <egor.shelemetev228@mail.ru>
2025-02-02 18:21:45 +1000

    fix

--- js/script.js
+++ js/script.js
@@ -3,6 +3,12 @@ const header = document.querySelector('.header');
 
 const infoBlock = document.querySelectorAll('.info__block')
 
+const btn = document.querySelector('.header__btn')
+
+let active = false
+
+var oldScrollY = 0;
+
 window.onload = () => {
     const reviewsSlider = new Swiper('.reviews__slider', {
         slidesPerView: 2,
@@ -25,13 +31,29 @@ window.onload = () => {
         }
     })
 
-    window.addEventListener("wheel", (evnt) => {
-        if (evnt.deltaY > 0) {
-            header.classList.add('header--hidden')
+    btn.addEventListener("click" ,() => {
+        if (active) {
+            menu.close()
+            active = false
         } else {
-            header.classList.remove('header--hidden')
+            menu.show()
+            active = true
         }
     })
+
+    window.addEventListener('scroll', function(){
+        var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+        var dY = scrolled - oldScrollY;
+        
+        if ( dY > 0 ){
+          header.classList.add('header--hidden');
+          menu.close()
+        } else {
+            header.classList.remove('header--hidden');
+        }
+        
+        oldScrollY = scrolled;
+    });
 
     infoBlock.forEach((item) => {
         item.addEventListener('click', (event) => {
